6232aa3b6e3e4cd276a9d23c55c2bbfe0ec85fd1
Aziz Mevlana Alim <147847410+aziz-mevlana@users.noreply.github.com>
2024-12-21 00:47:15 +0300

    v0.6
    chat ui update

--- student_tracking/chat/models.py
+++ student_tracking/chat/models.py
@@ -10,7 +10,6 @@ class Room(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     first_user=models.ForeignKey(User,related_name="room_first",on_delete=models.CASCADE, null=True) #!null olmamali
     second_user=models.ForeignKey(User,related_name="room_second",on_delete=models.CASCADE, null=True) #!null olmamali
-
 # class ChatUser(models.Model): #?Grup Konusmasi icin sonradan duzenlenebilir
 #     user = models.ForeignKey(User, related_name="chat_user", verbose_name="Kullanıcı", on_delete=models.CASCADE)
 #     room = models.ForeignKey(Room, related_name="chat_users", verbose_name="Oda", on_delete=models.CASCADE)
